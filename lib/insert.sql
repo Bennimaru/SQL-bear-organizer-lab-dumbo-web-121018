5848a0cbf0c02c7465b584d33431bd306d637260
Bennimaru <bennywhyu@gamil.com>
2018-12-17 16:24:24 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Mr.Chocolate",5,"Male","dark brown","rowdy",1);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Rowdy",18,"Male","light brown","rowdy",1);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Tabitha",6,"Female","black","shy",1);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Sergeant Brown",25,"Male","light brown","aggresive",1);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Melissa",10,"Female","dark brown","neutral",0);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Grinch",8,"Male","dark brown","aggresive",0);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Wendy",21,"Female","black","neutral",1);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES (NULL,NULL,"Male","dark brown","aggresive",1);
